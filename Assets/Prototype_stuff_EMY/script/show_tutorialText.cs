e6425187c1030d8eeac90ef79a6ca727066570c9
EMYGP\EmyGP <emyburgp@gmail.com>
2018-12-09 20:51:47 +0100

    aggiunta cartella prototype_stuff per test di design

---
+++ Assets/Prototype_stuff_EMY/script/show_tutorialText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class show_tutorialText : MonoBehaviour {
+
+    public GameObject tutorial_text;
+    private Collider tutorial_cube_collider;
+
+	void Start () {
+        tutorial_text.SetActive(false);
+        tutorial_cube_collider = gameObject.GetComponent<Collider>();
+	}
+
+    private void OnTriggerEnter(Collider tutorial_cube_collider) //ontriggerstay where are you?
+    {
+        tutorial_text.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider tutorial_cube_collider)
+    {
+        tutorial_text.SetActive(false);
+    }
+}
