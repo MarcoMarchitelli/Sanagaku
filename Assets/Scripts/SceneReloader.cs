137fa12b266b3884bc1e493d1e842a2db1e1c910
Marco Marchitelli <marchitelli97@libero.it>
2018-12-09 19:18:47 +0100

    Aggiunta possibilità di restart scene.

---
+++ Assets/Scripts/SceneReloader.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneReloader : MonoBehaviour {
+
+    public KeyCode ReloadKey;
+	
+	void Update () {
+        if (Input.GetKeyDown(ReloadKey))
+        {
+            RealoadScene();
+        }
+	}
+
+    void RealoadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
