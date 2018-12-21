bb81bc6da856157a4e3789405291a89b2c2a1673
DESKTOP-HBM023S\Davide <mongadavide@gmail.com>
2018-12-21 18:23:35 +0100

    more work and no beer

--- Assets/Prototype_02/Scripts_02/Spawn1.cs
+++ Assets/Prototype_02/Scripts_02/Spawn1.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class Spawn1 : MonoBehaviour {
 
+    public GameObject[] enemiesSpawn1;
+    private bool isTriggered;
+
+
 	// Use this for initialization
 	void Start () {
-		
+
+        isTriggered = false;
+
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void OnTriggerEnter (Collider other)
+    {
+		if  (other.gameObject.tag == "Player" && isTriggered == false)
+        {
+            
+            isTriggered = true;
+            for (int i = 0; i < enemiesSpawn1.Length; i++)
+            {
+                enemiesSpawn1[i].SetActive(true);
+            }
+        }
 	}
 }
