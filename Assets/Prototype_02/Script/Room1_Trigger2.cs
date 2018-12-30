df1493a53d0f9e39cdd1db5a1618bb4b1f207ef9
EMYGP\EmyGP <emyburgp@gmail.com>
2018-12-30 15:58:47 +0100

    Trigger from Room 1 completed: now they have to be hit in order to open the door (no animation). Minor changes on Prototype_02 scene. All new scripts are in a new folder, no changes on older scripts.

---
+++ Assets/Prototype_02/Script/Room1_Trigger2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room1_Trigger2 : MonoBehaviour {
+
+    //public bool trigger_b1 = false;
+    Color colorOff;
+    Color colorOn;
+    public Light TriggerLight;
+    public GameObject triggerManager;
+
+    void Start ()
+    {
+        //trigger_b1 = false;
+        colorOff = new Color(0.84f, 0.16f, 0.10f, 1f);
+        colorOn = new Color(0.10f, 0.84f, 0.35f, 1f);
+        TriggerLight.color = colorOff;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "PlayerBullet" && triggerManager.GetComponent<Room1_triggerManager>().trigger_b0 == true && triggerManager.GetComponent<Room1_triggerManager>().trigger_b1 == true)
+        {
+            TriggerLight.color = colorOn;
+            Debug.Log("THREEEEEEEE");
+            triggerManager.GetComponent<Room1_triggerManager>().trigger_b2 = true;
+        }
+        else { }
+    }
+}
