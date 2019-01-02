554b346ab65a0151a2d41b96c748c2e219181286
Marco Marchitelli <marchitelli97@libero.it>
2019-01-02 23:50:14 +0100

    Fixati vari errori dovuti a reference mancanti.

--- Assets/Prototype_02/Scripts_02/TestEnemyNew.cs
+++ Assets/Prototype_02/Scripts_02/TestEnemyNew.cs
@@ -58,7 +58,7 @@ public class TestEnemyNew : BaseUnit
     {
         if (path)
             StartCoroutine(FollowPathAnim(MoveSpeed, waitTime));
-        if(DiesFromBounces)
+        if(DiesFromBounces && BouncesNeededToDie > 0)
             material.color = ColorContainer.Instance.Colors[BouncesNeededToDie - 1];
     }
 
