7a1bd9db1346d92786d1780dfd76e7925074179e
EMYGP\EmyGP <emyburgp@gmail.com>
2019-01-04 11:02:24 +0100

    Added a script for door managment and a script to teleport the player. Minor changes on Prototype_02 (3) scene

---
+++ Assets/Prototype_02/Script/DoorCloser_script.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorCloser_script : MonoBehaviour {
+
+    public GameObject Door;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Door.gameObject.SetActive(false);	
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+       if (other.tag == "Player")
+        {
+            Door.gameObject.SetActive(true);
+        }
+    }
+
+}
---
+++ Assets/Prototype_02/Script/Teleport_player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport_player : MonoBehaviour {
+    public GameObject player;
+    public GameObject spawn_point;
+    //public Vector3 teleport_point;
+    public Collider teleport_trigger;
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            player.transform.localPosition = spawn_point.transform.localPosition;
+        }
+    }
+
+
+}
