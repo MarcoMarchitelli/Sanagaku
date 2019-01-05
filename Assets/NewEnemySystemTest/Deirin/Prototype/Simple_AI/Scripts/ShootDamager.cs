687bef3d4aa494c532b26305a01ba9148a06ec91
Marco Marchitelli <marchitelli97@libero.it>
2019-01-05 01:59:25 +0100

    Revert "Revert "Player now gets damaged by enemy projectiles, added counters.""
    This reverts commit 20dd45cf25487f178f92d991d38f26f4e1df5cdf.

---
+++ Assets/NewEnemySystemTest/Deirin/Prototype/Simple_AI/Scripts/ShootDamager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Deirin.AI
+{
+    public class ShootDamager : MonoBehaviour
+    {
+
+        public int damage = 1;
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            ShootTarget t = collision.collider.GetComponent<ShootTarget>();
+            if (t)
+                t.TakeDamage(damage);
+        }
+    }
+}
