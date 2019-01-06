180c444f24b517c18fda67ceed47e390e46f5837
DESKTOP-HBM023S\Davide <mongadavide@gmail.com>
2019-01-06 18:14:51 +0100

    moar polishing. Aggiunto un minimo di tutorial UI.

---
+++ Assets/Prototype_02/Scripts_02/UIAdvice.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIAdvice : MonoBehaviour {
+
+    public TextMeshProUGUI adviceText;
+
+    void Start()
+    {
+        adviceText.enabled = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            adviceText.enabled = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            adviceText.enabled = false;
+        }
+    }
+}
---
+++ Assets/Prototype_02/Scripts_02/WinAudio.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinAudio : MonoBehaviour {
+
+    public AudioClip winAudioSample;
+    AudioSource audio;
+    public float Volume;
+    public bool alreadyPlayed = false;
+
+	
+	void Start () {
+
+        audio = GetComponent<AudioSource>();
+        
+	}
+	
+	void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player") && !alreadyPlayed)
+        {
+            audio.PlayOneShot(winAudioSample, Volume);
+            alreadyPlayed = true;
+        }
+    }
+}
