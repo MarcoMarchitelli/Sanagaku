888ba648b4946581128156f69f37ab8ad4490a4f
Luca <lu.gbelli@hotmail.it>
2019-01-17 18:16:03 +0100

    Aggiunte interfacce

---
+++ Assets/Scripts/Interfaces/IBehaviour.cs
@@ -0,0 +1,19 @@
+﻿namespace Sangaku
+{
+    /// <summary>
+    /// Interfaccia che definisce un comportamento
+    /// </summary>
+    public interface IBehaviour
+    {
+        IEntity Entity { get; }
+
+        /// <summary>
+        /// Funzione che avvia una Behaviour
+        /// </summary>
+        void StartBehaviour();
+        /// <summary>
+        /// Funzione che ferma una Behaviour
+        /// </summary>
+        void StopBehaviour();
+    } 
+}
---
+++ Assets/Scripts/Interfaces/IEntity.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Sangaku
+{
+    /// <summary>
+    /// Interfaccia che definisce il comportamento di un'entità
+    /// </summary>
+    public interface IEntity
+    {
+        /// <summary>
+        /// Lista di Behaviour dell'entità
+        /// </summary>
+        List<IBehaviour> Behaviours { get; }
+    }
+}
