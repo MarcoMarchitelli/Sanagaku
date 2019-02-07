1f316ad957641577d31bea4063fcdd34e577be5e
Luca Garbelli <lu.gbelli@hotmail.it>
2019-02-07 13:14:47 +0100

    Minor fix

---
+++ Assets/Scripts/Behaviours/Custom/FollowerBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Sangaku
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class FollowerBehaviour : BaseBehaviour
+    {
+
+    } 
+}
