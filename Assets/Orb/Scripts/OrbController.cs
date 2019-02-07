53056287b9250ae39630afdda4c9b302e5c0f28d
Luca Garbelli <lu.gbelli@hotmail.it>
2019-02-07 11:51:31 +0100

    Uber order

---
+++ Assets/Orb/Scripts/OrbController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Sangaku
+{
+    public class OrbController : BaseEntity
+    {
+        public OrbSMController SM;
+
+        //Transform catchPoint;
+
+        //public void OrbSetUp(Transform _catchPoint)
+        //{
+        //    catchPoint = _catchPoint;
+        //    SetUpEntity();
+        //}
+
+        public override void CustomSetup()
+        {
+            if(!SM)
+                SM = GetComponentInChildren<OrbSMController>();
+            //SM.OrbSMSetUp(catchPoint);
+            SM.SetUpSM();
+        }
+    }
+}
