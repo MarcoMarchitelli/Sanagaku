3137cbc4c0a0c3df06f34d4f1bc894503a7dd088
Luca Garbelli <lu.gbelli@hotmail.it>
2019-02-26 15:14:02 +0100

    Extended base classes functionality

--- Assets/Scripts/Interfaces/IEntity.cs
+++ Assets/Scripts/Interfaces/IEntity.cs
@@ -12,19 +12,38 @@ namespace Sangaku
         /// Lista di Behaviour dell'entità
         /// </summary>
         List<IBehaviour> Behaviours { get; }
-
+        /// <summary>
+        /// Riferimento al proprio game object
+        /// </summary>
         GameObject gameObject { get; }
-
+        /// <summary>
+        /// Riferimento al dato dell'entità
+        /// </summary>
         IEntityData Data { get; }
 
+        /// <summary>
+        /// Funzione di setup dell'entità
+        /// </summary>
         void SetUpEntity();
+
+        /// <summary>
+        /// Entity's custom update.
+        /// </summary>
+        void OnUpdate();
+
+        /// <summary>
+        /// Entity's custom fixed update.
+        /// </summary>
+        void OnFixedUpdate();
+
+        /// <summary>
+        /// Entity's custom late update.
+        /// </summary>
+        void OnLateUpdate();
     }
 
     /// <summary>
     /// Data needed for the entity to work, that comes from outside the entity's behaviours or the entity itself.
     /// </summary>
-    public interface IEntityData
-    {
-
-    }
+    public interface IEntityData { }
 }
