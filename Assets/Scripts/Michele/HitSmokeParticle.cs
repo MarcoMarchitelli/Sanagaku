8890f387f416825c2df463176d3c3938027fcb06
Michele Pesante <pesante.michele.96@gmail.com>
2019-03-06 01:58:05 +0100

    Creati particle nella pool con relativa logica di tipo HitSmoke

---
+++ Assets/Scripts/Michele/HitSmokeParticle.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace Sangaku
+{
+    /// <summary>
+    /// Script dell'oggetto HitSmoke che è poolabile
+    /// </summary>
+    public class HitSmokeParticle : BaseEntity, IPoolable
+    {
+        ParticleSystem particle;
+        DestroyBehaviour destroyBehaviour;
+        
+        public override void CustomSetup()
+        {
+            particle = GetComponent<ParticleSystem>();
+            ParticleSystem.MainModule main = particle.main;
+            main.stopAction = ParticleSystemStopAction.Callback; // Serve per chiamare l'evento OnParticleSystemStopped
+            destroyBehaviour = GetComponent<DestroyBehaviour>();
+        }
+
+        public void OnGetFromPool()
+        {
+            SetUpEntity();
+            particle.Play();
+        }
+
+        public void OnPoolCreation()
+        {
+
+        }
+
+        public void OnPutInPool()
+        {
+
+        }
+
+        /// <summary>
+        /// Evento chiamato alla fine del ciclo completo di vita di un particle system
+        /// </summary>
+        public void OnParticleSystemStopped()
+        {
+            destroyBehaviour.Destroy();
+        }
+    }
+}
---
+++ Assets/Scripts/Michele/ParticleSpawner.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sangaku
+{
+    /// <summary>
+    /// Spawner che gestisce le fasi del particle system che ha in gestione
+    /// </summary>
+    public class ParticleSpawner : MonoBehaviour
+    {
+        HitSmokeParticle currentActiveParticle;
+
+        [SerializeField] protected string particlePool = "HitSmoke";
+
+        #region Events
+        public UnityVoidEvent OnParticleStart;
+        public UnityVoidEvent OnParticleEnd;
+        #endregion
+
+        /// <summary>
+        /// Funzione che esegue prima l'evento di start, poi il play della particle
+        /// </summary>
+        public void PlayParticle()
+        {
+            OnParticleStart.Invoke();
+            currentActiveParticle = ObjectPooler.Instance.GetPoolableFromPool(particlePool, transform.position, transform.rotation) as HitSmokeParticle;
+        }
+
+        /// <summary>
+        /// Funzione che esegue prima lo stop, poi l'evento di end della particle
+        /// </summary>
+        public void StopParticle()
+        {
+            ObjectPooler.Instance.PutPoolableInPool(particlePool, currentActiveParticle);
+            OnParticleEnd.Invoke();
+        }
+
+        #region Debug
+        // REGION PER LE PROVE DI DEBUG NELLA SCENA MICHELETEST!!!
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                PlayParticle();
+            }
+
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                StopParticle();
+            }
+        }
+        #endregion
+    }
+}
