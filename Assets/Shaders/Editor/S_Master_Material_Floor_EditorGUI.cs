4e6d4375a4f5c86889edd00481d7dad85ca3df56
Luca Garbelli <lu.gbelli@hotmail.it>
2019-04-09 12:55:48 +0200

    Test custom editor shader

---
+++ Assets/Shaders/Editor/S_Master_Material_Floor_EditorGUI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class S_Master_Material_Floor_EditorGUI : ShaderGUI
+{
+    // link spiegone : https://catlikecoding.com/unity/tutorials/rendering/part-9/
+
+    MaterialEditor materialEditor;
+    MaterialProperty[] properties;
+
+    public override void OnGUI(MaterialEditor _materialEditor, MaterialProperty[] _properties)
+    {
+        materialEditor = _materialEditor;
+        properties = _properties;
+        DoMain();
+    }
+
+    void DoMain()
+    {
+        GUILayout.Label("Main Maps", EditorStyles.boldLabel);
+
+        MaterialProperty mainTex = FindProperty("_MainTex", properties);
+        GUIContent albedoLabel = new GUIContent(mainTex.displayName);
+        materialEditor.TexturePropertySingleLine(albedoLabel, mainTex);
+    }
+}
