4cdc436dfccc97ed3ca9e4f5c4e0751e9de3d2e1
DESKTOP-19FL4E5\PC <massi1996pa@gmail.com>
2019-04-11 13:23:48 +0200

    fix schermate tutorial/ aggiunta HUD boss provvisoria (per torino)

---
+++ Assets/UI/Scripts/HealthUITest1.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Sangaku;
+using System.Collections.Generic;
+
+public class HealthUITest1 : MonoBehaviour
+{
+    [SerializeField] List<GameObject> healthbar;
+    [SerializeField] DamageReceiverBehaviour damageReceiver;
+
+    public void SetUp(DamageReceiverBehaviour _drb = null)
+    {
+        if (!damageReceiver && _drb)
+            damageReceiver = _drb;
+
+        damageReceiver.OnHealthChanged.AddListener(UpdateUI);
+    }
+
+    public void UpdateUI(int _newHealth)
+    {
+        for (int i = 0; i < healthbar.Count; i++)
+        {
+            if (i >= _newHealth)
+            {
+                healthbar[i].SetActive(false);
+            }
+            else
+            {
+                healthbar[i].SetActive(true);
+            }
+        }
+    }
+
+    //TEMPORARY
+    private void Start()
+    {
+        SetUp();
+    }
+
+}
