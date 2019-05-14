06b09fb1ab13e3e1dce2592086003823edec6900
Fulvio <montecristo310@gmail.com>
2019-05-14 11:21:48 +0200

    Sostituita immagine UI
    - Sostituita barra vita
    - Aggiunta logica per aggiornare la visualizzazione della vita

---
+++ Assets/UI/Scripts/UI_HealthBehaviour.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Sangaku
+{
+    public class UI_HealthBehaviour : BaseBehaviour
+    {
+        DamageReceiverBehaviour damageReceiver;
+        List<Image> healthImages = new List<Image>();
+
+        public void CustomSetup(DamageReceiverBehaviour demageBehaviour)
+        {
+            AddHealthBars();
+
+            if (!damageReceiver && demageBehaviour)
+                damageReceiver = demageBehaviour;
+
+            damageReceiver.OnHealthChanged.AddListener(UpdateUI);
+        }
+
+        public override void OnLateUpdate()
+        {
+            transform.rotation = Quaternion.Euler(-90, 0, 180);
+        }
+
+        /// <summary>
+        /// Prende le barre della vita figlie e le salva (esclude la prima immagine in quanto background
+        /// </summary>
+        void AddHealthBars()
+        {
+            Image[] images = GetComponentsInChildren<Image>();
+
+            for (int i = 1; i < images.Length; i++)
+                healthImages.Add(images[i]);
+        }
+        
+        /// <summary>
+        /// spegne e accende le barre della vita
+        /// </summary>
+        /// <param name="_newHealth"></param>
+        public void UpdateUI(int _newHealth)
+        {
+            for (int i = 0; i < healthImages.Count; i++)
+            {
+                if (i >= _newHealth)
+                {
+                    healthImages[i].enabled = false;
+                }
+                else
+                {
+                    healthImages[i].enabled = true;
+                }
+            }
+        }
+
+    }
+}
