b43fa2b37362ee22847f17605e33d3e85ecfa8ea
DESKTOP-HBM023S\Davide <mongadavide@gmail.com>
2019-05-15 13:04:45 +0200

    boss redone + new boss weapon WIP

---
+++ Assets/BossGrenadeAnim.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public class BossGrenadeAnim : MonoBehaviour
+    {
+        /// <summary>
+        /// Riferimento all'animator
+        /// </summary>
+        [SerializeField]
+        Animator ExpAnimation;
+
+        /// <summary>
+        /// Funzione che attiva il dissolve
+        /// </summary>
+        /// <param name="_other"></param>
+        public void StartExpAnimation()
+        {
+            ExpAnimation.SetBool("PlayExp", true);
+        }
+
+        public void ResetExpAnimation()
+        {
+            ExpAnimation.SetBool("PlayExp", false);
+        }
+
+    }   
