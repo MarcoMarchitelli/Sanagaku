8703364eca9e0cecb093c8a10a2215e9445c4560
Fulvio <montecristo310@gmail.com>
2019-05-27 13:01:42 +0200

    Enemy animator controller e Fix animazioni player
    - Aggiunto il controller del nemico per le animazioni
    - Piccoli fix sulla gestione delle animazioni player

---
+++ Assets/Enemies/Scripts/EnemyAnimatorBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sangaku
+{
+    public class EnemyAnimatorBehaviour : BaseBehaviour
+    {
+
+        Animator anim;
+
+        protected override void CustomSetup()
+        {
+            anim = GetComponent<Animator>();
+        }
+
+        public void SetDamageAnimation()
+        {
+            anim.SetTrigger("TakeDamage");
+        }
+
+        public void SetShotAnimation()
+        {
+            anim.SetTrigger("Shot");
+        }
+    } 
+}
