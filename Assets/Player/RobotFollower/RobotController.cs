e6c877c55a158464fd6429230881d5460e0a1061
Fulvio <montecristo310@gmail.com>
2019-06-02 15:25:37 +0200

    movimento robot e morte nemico
    - Aggiunto movimento per il robot che segue il player.
    - Aggiunta animazione shader del nemico follower

---
+++ Assets/Player/RobotFollower/RobotController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sangaku
+{
+    public class RobotController : BaseEntity
+    {
+        public void Init()
+        {
+
+        }
+    } 
+}
---
+++ Assets/Player/RobotFollower/RobotMovementBehaviour.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sangaku
+{
+    public class RobotMovementBehaviour : MonoBehaviour
+    {
+        Vector3 direction;
+        Rigidbody rBody;
+        float moveSpeed;
+
+        public bool IsRbodyMovement;
+
+        [Header("Transform movement")]
+        public float LerpTime;
+
+        [Header("General")]
+        public Transform Target;
+        public float DistanceFromTarget;
+        public Vector3 Offset;
+
+        private void Start()
+        {
+            Init();
+        }
+
+        public void Init()
+        {
+            moveSpeed = 12;
+            rBody = GetComponent<Rigidbody>();
+        }
+
+        private void LateUpdate()
+        {
+            if(transform.position != Target.position - Offset)
+                Move(GetTargetDirection());
+        }
+
+        Vector3 GetTargetDirection()
+        {
+            return new Vector3(Target.position.x - transform.position.x, Target.position.y - transform.position.y, Target.position.z - transform.position.z);
+        }
+        
+        public void Move(Vector3 _direction)
+        {
+            if(IsRbodyMovement)
+                rBody.MovePosition(rBody.position + _direction * moveSpeed * Time.deltaTime);
+            else
+                transform.position = Vector3.Lerp(transform.position, Target.position - Offset, LerpTime);
+        }
+
+    }
+}
