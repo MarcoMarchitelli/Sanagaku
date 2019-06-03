eb7395dba2bf4990bb8371351a63f053488c1700
Luca Garbelli <lu.gbelli@hotmail.it>
2019-06-03 13:33:00 +0200

    Rifatti menu

---
+++ Assets/UI/Scripts/ButtonHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+namespace Sangaku
+{
+    [RequireComponent(typeof(Button))]
+    public class ButtonHandler : MonoBehaviour, ISelectHandler, IDeselectHandler
+    {
+        [SerializeField] UnityVoidEvent OnButtonSelected;
+        [SerializeField] UnityVoidEvent OnButtonDeselect;
+
+        Button button;
+
+        public void OnSelect(BaseEventData eventData)
+        {
+            OnButtonSelected.Invoke();
+        }
+
+        public void OnDeselect(BaseEventData eventData)
+        {
+            OnButtonDeselect.Invoke();
+        }
+
+        void Start()
+        {
+            button = GetComponent<Button>();
+        }
+    }
+}
