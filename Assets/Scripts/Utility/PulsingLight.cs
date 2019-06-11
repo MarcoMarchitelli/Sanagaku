dbbf0aba5be16ba013e1014e377b3fb81132dc66
Luca Garbelli <lu.gbelli@hotmail.it>
2019-06-11 11:46:30 +0200

    pulizia file + script luce pulsante

---
+++ Assets/Scripts/Utility/PulsingLight.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+public class PulsingLight : MonoBehaviour
+{
+    public float maxIntensity = 1f;
+    public float minIntensity = 0f;
+    public float pulseSpeed = 1f; //here, a value of 0.5f would take 2 seconds and a value of 2f would take half a second
+
+    new private Light light;
+    private float targetIntensity = 1f;
+    private float currentIntensity;
+
+    void Start()
+    {
+        light = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        currentIntensity = Mathf.MoveTowards(light.intensity, targetIntensity, Time.deltaTime * pulseSpeed);
+        if (currentIntensity >= maxIntensity)
+        {
+            currentIntensity = maxIntensity;
+            targetIntensity = minIntensity;
+        }
+        else if (currentIntensity <= minIntensity)
+        {
+            currentIntensity = minIntensity;
+            targetIntensity = maxIntensity;
+        }
+        light.intensity = currentIntensity;
+    }
+}
