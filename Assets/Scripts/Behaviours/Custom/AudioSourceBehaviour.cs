f46b6560a67eb7551cc309f9b5522243fd254b26
Luca Garbelli <lu.gbelli@hotmail.it>
2019-07-01 17:06:16 +0200

    Aggiunto audio source behaviour

---
+++ Assets/Scripts/Behaviours/Custom/AudioSourceBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Sangaku
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class AudioSourceBehaviour : MonoBehaviour
+    {
+        [SerializeField] UnityVoidEvent OnStopPlaying;
+
+        AudioSource source;
+
+        private void Start()
+        {
+            source = GetComponent<AudioSource>();
+            StartCoroutine(WaitForSound());
+        }
+
+        public IEnumerator WaitForSound()
+        {
+            if (source != null)
+            {
+                yield return new WaitWhile(() => source.isPlaying == true);
+                OnStopPlaying.Invoke();
+            }
+        }
+    }
+}
