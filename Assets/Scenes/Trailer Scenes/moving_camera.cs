6df5da2301b4592ac42fbf6d8cd5dd3e33f0e8d5
SUCC_MI_12 <ehmi@gmail.com>
2019-07-03 16:59:45 +0200

    aggiunti 2 input nei project settings, aggiunta scena trailer

---
+++ Assets/Scenes/Trailer Scenes/moving_camera.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class moving_camera : MonoBehaviour
+{
+    public float speed;
+    public float rSpeed;
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        float moveUpD = Input.GetAxis("Debug_UpDown");
+
+        float rotateSxDx = 1.0f;
+
+        Vector3 movement = new Vector3(moveHorizontal, moveUpD, moveVertical);
+        Quaternion rotation = new Quaternion(0.0f, rotateSxDx * rSpeed, 0.0f, 0.0f);
+
+
+        GetComponent<Rigidbody>().velocity = movement * speed;
+
+        if (Input.GetKeyDown("o"))
+        {
+            GetComponent<Transform>().rotation = rotation;
+        }
+    }
+}
