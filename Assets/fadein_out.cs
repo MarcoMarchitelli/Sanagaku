874760e5c17dfc0842280e5b1e4da7afed97354c
DESKTOP-HBM023S\Davide <mongadavide@gmail.com>
2019-07-05 17:06:29 +0200

    more work on comics

---
+++ Assets/fadein_out.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fadein_out : MonoBehaviour
+{
+    public Animator animator;    
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FadeOut()
+    {
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void FadeIn()
+    {
+        animator.SetTrigger("FadeIn");
+    }
+}
