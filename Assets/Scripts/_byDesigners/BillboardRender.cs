ef81af66efe88be2f2b136a175b30ed4f2b43073
DESKTOP-HBM023S\Davide <mongadavide@gmail.com>
2019-07-17 17:04:03 +0200

    more polishing

---
+++ Assets/Scripts/_byDesigners/BillboardRender.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillboardRender : MonoBehaviour
+{
+
+    void Update()
+    {
+
+        transform.LookAt(Camera.main.transform.position, -Vector3.up);
+
+    }
+
+}
+
+
