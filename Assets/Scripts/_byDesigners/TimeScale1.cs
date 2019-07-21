672fd0638f2de99fbdee1d95922707e7ea39c053
DESKTOP-HBM023S\Davide <mongadavide@gmail.com>
2019-07-21 19:58:42 +0200

    Intro video

---
+++ Assets/Scripts/_byDesigners/TimeScale1.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeScale1 : MonoBehaviour
+{
+
+    public void StartTime ()
+    {
+    
+        Time.timeScale = 1;
+
+    }
+
+}
