b38a157094f0c15bf6050f1fc12a402c992eff50
DESKTOP-HBM023S\Davide <mongadavide@gmail.com>
2019-07-22 08:25:03 +0200

    boss death / hurt animation

---
+++ Assets/Scripts/_byDesigners/BossDeathParticlesAnim.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossDeathParticlesAnim : MonoBehaviour
+{
+    public Animator animator;    
+          
+    public void DeathStart()
+    {
+        animator.SetTrigger("BossDeath");
+    }
+
+
+}
