4987f875a30bcb33d8c8d5a8b9aada95cb6ee0c0
DESKTOP-HBM023S\Davide <mongadavide@gmail.com>
2019-07-22 05:52:15 +0200

    More fixes

---
+++ Assets/Scripts/_byDesigners/PlayerControllerToggle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerControllerToggle : MonoBehaviour
+{
+    //private GameObject gameManager;
+    
+
+
+    //public void PlayerControlOff ()
+    //{
+    //    gameManager = GameObject.FindWithTag("GameManager");
+    //    Controller = gameManager.GetComponent<TemporaryGameManager>();
+
+        
+
+    //}
+
+    
+}
---
+++ Assets/Scripts/_byDesigners/TimeScaleChanger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeScaleChanger : MonoBehaviour
+{
+
+    public void StartTime ()
+    {
+    
+        Time.timeScale = 1;
+
+    }
+
+    public void StopTime ()
+    {
+
+        Time.timeScale = 0;
+
+    }
+
+}
