7560b5604c2dbcc47e3f3a48b4d859a9f1f8d568
DESKTOP-HBM023S\Davide <mongadavide@gmail.com>
2019-07-22 15:23:24 +0200

    Almost there (Audio SFX vari, Credits, more bugfixes)

---
+++ Assets/Scripts/_byDesigners/DoorSFX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorSFX : MonoBehaviour
+{
+    public AudioSource OpenDoor;
+    public AudioSource CloseDoor;
+
+    public void PlayOpen()
+    {
+        OpenDoor.Play();
+
+    }
+
+    public void PlayClose()
+    {
+        CloseDoor.Play();
+
+    }
+
+}
